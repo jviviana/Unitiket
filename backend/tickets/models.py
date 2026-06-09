e7b6ebe47e3b16b4207d5af244773e0962a40d9d
Jessica <jessica.viscue@correounivalle.edu.co>
2026-06-08 20:57:54 -0500

    corregir espacios en blanco, etc

--- backend/tickets/models.py
+++ backend/tickets/models.py
@@ -9,6 +9,7 @@ class User(AbstractUser):
     )
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='student')
 
+
 class Ticket(models.Model):
     STATUS_CHOICES = (
         ('OPEN', 'Abierto'),
